21d244a370d8df22360b648c7b94086fa827c7c3
soyeonnnb <la28s5d@naver.com>
2023-05-23 17:51:49 +0900

    docs: 안쓰는 파일 삭제

--- src/main/java/com/ssafy/model/dao/challenge/ChallengeContentDao.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.model.dao.challenge;
-
-import com.ssafy.model.dto.challenge.ChallengeContent;
-
-public interface ChallengeContentDao {
-	int insert(ChallengeContent challengeContent);
-	int update(ChallengeContent challengeContent);
-	int delete(int id);
-}
